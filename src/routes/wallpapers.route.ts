1706eb02107e154b1a112ae192bc133a0b3b9dcb
gwythyr9 <56584390+gwythyr9@users.noreply.github.com>
2023-06-04 21:01:49 +0300

    fix route

--- src/routes/wallpapers.route.ts
+++ src/routes/wallpapers.route.ts
@@ -15,7 +15,7 @@ export class WallpaperRoute implements Routes {
 
   private initializeRoutes() {
     this.router.get(`${this.path}`, this.wallpaper.getWallpapers);
-    this.router.get(`${this.path}/:id`, this.wallpaper.getWallpaperById);
+    this.router.get(`${this.path}/:name`, this.wallpaper.getWallpaperByName);
     this.router.post(`${this.path}`, ValidationMiddleware(CreateWallpaperDto), this.wallpaper.createWallpaper);
     this.router.put(`${this.path}/:id`, ValidationMiddleware(CreateWallpaperDto, false, true), this.wallpaper.updateWallpaper);
     this.router.delete(`${this.path}/:id`, this.wallpaper.deleteWallpaper);
