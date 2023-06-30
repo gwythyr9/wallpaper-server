be4cae994bf3fe809a5522a27a9b146b56e8e8d6
gwythyr9 <56584390+gwythyr9@users.noreply.github.com>
2023-06-30 16:45:41 +0700

    digitalocean

--- src/jobs/wallpapers.jobs.ts
+++ src/jobs/wallpapers.jobs.ts
@@ -6,28 +6,35 @@ import { Wallpaper } from '@interfaces/wallpapers.interface';
 
 class WallpapersJobs {
   public refreshUrls() {
-    schedule.scheduleJob('*/5 * * * *', async () => {
+    schedule.scheduleJob('*/1 * * * *', async () => {
       const REGION = 'us-east-1';
-      const BUCKET = 'lookoutvision-us-east-1-12447157cd';
-      const client = new S3Client({ region: REGION });
+      const BUCKET = 'wallpapersapp';
+      const client = new S3Client({
+        endpoint: 'https://sfo3.digitaloceanspaces.com',
+        region: REGION,
+        credentials: {
+          accessKeyId: 'DO00WR3YRMTC42HKG2XN',
+          secretAccessKey: 'Gvqv/EuDiw0new4iV2MPMz1oonTxo7fjKW4W+wVoDVM',
+        },
+      });
       const getListOfVideo = new ListObjectsV2Command({
         Bucket: BUCKET,
-        Prefix: 'wallpapers/video/video',
+        Prefix: 'video/video',
         MaxKeys: 1000,
       });
       const getListOfImage = new ListObjectsV2Command({
         Bucket: BUCKET,
-        Prefix: 'wallpapers/image',
+        Prefix: 'image',
         MaxKeys: 1000,
       });
       const getListOfTheme = new ListObjectsV2Command({
         Bucket: BUCKET,
-        Prefix: 'wallpapers/theme/theme',
+        Prefix: 'theme/theme',
         MaxKeys: 1000,
       });
       const getListOfParallaxFolders = new ListObjectsV2Command({
         Bucket: BUCKET,
-        Prefix: 'wallpapers/parallax/',
+        Prefix: 'parallax/',
         Delimiter: '/',
         MaxKeys: 1000,
       });
@@ -35,13 +42,13 @@ class WallpapersJobs {
       const allWallpapers: Wallpaper[] = [];
       try {
         const { Contents: theme } = await client.send(getListOfTheme);
-        theme.shift();
+        console.log(theme);
+        // theme.shift();
         for (const imageTheme of theme) {
-          const imageKey = imageTheme.Key.replace('wallpapers/', '');
+          const imageKey = imageTheme.Key.replace('wallpapersapp/', '');
           const imageSplit = imageKey.split('/');
-          console.log('file: wallpapers.jobs.ts:42 ~ WallpapersJobs ~ schedule.scheduleJob ~ imageSplit:', imageSplit);
           const wallpaperData: Wallpaper = {
-            url: [`https://lookoutvision-us-east-1-12447157cd.s3.amazonaws.com/${imageTheme.Key}`],
+            url: [`https://wallpapersapp.sfo3.cdn.digitaloceanspaces.com/${imageTheme.Key}`],
             type: 'theme',
             subtype: 'theme',
             name: imageSplit[2],
@@ -52,10 +59,10 @@ class WallpapersJobs {
         const { Contents: videos } = await client.send(getListOfVideo);
         videos.shift();
         for (const video of videos) {
-          const videoKey = video.Key.replace('wallpapers/', '');
+          const videoKey = video.Key.replace('wallpapersapp/', '');
           const videoSplit = videoKey.split('/');
           const wallpaperData: Wallpaper = {
-            url: [`https://lookoutvision-us-east-1-12447157cd.s3.amazonaws.com/${video.Key}`],
+            url: [`https://wallpapersapp.sfo3.cdn.digitaloceanspaces.com/${video.Key}`],
             type: 'video',
             subtype: 'video',
             name: videoSplit[2],
@@ -67,15 +74,14 @@ class WallpapersJobs {
         images.shift();
         const filtredImages = images.filter(i => !i.Key.endsWith('/'));
         for (const image of filtredImages) {
-          const imageKey = image.Key.replace('wallpapers/', '');
+          const imageKey = image.Key.replace('wallpapersapp/', '');
           const imageSplit = imageKey.split('/');
           const wallpaperData: Wallpaper = {
-            url: [`https://lookoutvision-us-east-1-12447157cd.s3.amazonaws.com/${image.Key}`],
+            url: [`https://wallpapersapp.sfo3.cdn.digitaloceanspaces.com/${image.Key}`],
             type: 'image',
             subtype: imageSplit[1],
             name: imageSplit[2],
           };
-          // await wallpaper.createWallpaper(wallpaperData);
           allWallpapers.push(wallpaperData);
         }
         const response = await client.send(getListOfParallaxFolders);
@@ -87,8 +93,8 @@ class WallpapersJobs {
             MaxKeys: 1000,
           });
           const { Contents: parallaxFiles } = await client.send(getListFolderFiles);
-          const parallaxUrls = parallaxFiles.map(u => `https://lookoutvision-us-east-1-12447157cd.s3.amazonaws.com/${u.Key}`);
-          const imageKey = folder.replace('wallpapers/', '');
+          const parallaxUrls = parallaxFiles.map(u => `https://wallpapersapp.sfo3.cdn.digitaloceanspaces.com/${u.Key}`);
+          const imageKey = folder.replace('wallpapersapp/', '');
           const imageSplit = imageKey.split('/');
           const wallpaperData: Wallpaper = {
             url: parallaxUrls,
